bf27660ad2988f861f9d5ef31cb25d261a5a5f37
Alanis2 <alanisemanuela950@gmail.com>
2021-03-29 15:47:30 -0300

    Finalizado Operações Básicas
    As Operações Básicas De Matemáticas Estão Funcionando

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+var valor;
+var resultado;
+var calculado = false;
+
 function botao(num){
+    limpar()
     valor = document.calc.result.value += num;
+}
+
+function reseta(){
+    document.calc.result.value = '';
+    calculado = false
+}
+function calcula(){
+    var resultado = eval(valor);
+    document.calc.result.value = resultado.toLocaleString('pt-br');
+    calculado = true;
+}
+
+function limpar() {
+    if (calculado) {
+        reseta()        
+    }
 }
 
 
